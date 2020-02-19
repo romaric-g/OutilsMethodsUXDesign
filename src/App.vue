5cf0bd318809d6259bd75dbc51c19f72377c61af
LeoPhilip33 <56159507+LeoPhilip33@users.noreply.github.com>
2020-02-19 17:39:18 +0100

    Content add

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Home/>
+    <Content/>
     <p>Hello World! 000</p>
+    
   </div>
 </template>
 
 <script>
+import Content from "./components/content/Content"
 import Home from "./components/home/Home"
 
 export default {
   name: 'app',
-  components:  {Home},
+  components:  {Home ,Content},
   data () {
     return {
       
