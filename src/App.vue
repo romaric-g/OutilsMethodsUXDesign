5f56b0702b26d58080a8e2ffe57fa6e0454393b1
Sekori1 <romaric55220@gmail.com>
2020-02-19 14:50:38 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <p>Hello World!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
