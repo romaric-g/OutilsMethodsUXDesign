d81c1aad2c4e463d2e7458aab0c696f3cae8b463
Sekori1 <romaric55220@gmail.com>
2020-02-19 17:30:43 +0100

    Nav

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,14 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Montserrat:700&display=swap');
+body {
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
